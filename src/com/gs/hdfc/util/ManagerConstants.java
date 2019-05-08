6b3490e8f92f56eebcaba9baa65c4a62a1df5c14
Mohit Malhotra <mmalhotra2000@gmail.com>
2019-05-08 20:32:31 +0530

    Constants

--- src/com/gs/hdfc/util/ManagerConstants.java
+++ src/com/gs/hdfc/util/ManagerConstants.java
@@ -3,15 +3,15 @@ package com.gs.hdfc.util;
 public class ManagerConstants {
 
 	
-	public static String MANAGER_ID = "Manager Id";
-	public static String MANAGER_NAME = "Manager Name";
-	public static String MANAGER_AGE = "Manager Age";
-	public static String MANAGER_SALARY = "Manager Salary";
-	public static String MANAGER_GENDER = "Manager Gender";
-	public static String MANAGER_EXP = "Manager Years Of Exp";
-	public static String MANAGER_PROJECTS = "Manager Projects Accomplished";
-	public static String MANAGER_NUMBERS = "Manager Phone Numbers";
-	public static String PROJECT_NAME = "Project Name";
+	public static final String  MANAGER_ID = "Manager Id";
+	public static final String MANAGER_NAME = "Manager Name";
+	public static final String MANAGER_AGE = "Manager Age";
+	public static final String MANAGER_SALARY = "Manager Salary";
+	public static final String MANAGER_GENDER = "Manager Gender";
+	public static final String MANAGER_EXP = "Manager Years Of Exp";
+	public static final String MANAGER_PROJECTS = "Manager Projects Accomplished";
+	public static  final String MANAGER_NUMBERS = "Manager Phone Numbers";
+	public static final String PROJECT_NAME = "Project Name";
 	
 
 	
